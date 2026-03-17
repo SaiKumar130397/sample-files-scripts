22b8f5ee10786fdae5f4ec7839b1ed984cb8ea74
Sai Kumar <kumarsaisurisetty333@gmail.com>
2026-03-17 18:43:25 +0530

    modified bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -83,15 +83,15 @@ install_jenkins() {
     apt-get install -y openjdk-17-jre fontconfig
 
     curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key \
-        | tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+      | gpg --dearmor -o /usr/share/keyrings/jenkins-keyring.gpg
 
-    echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-        https://pkg.jenkins.io/debian-stable binary/ \
-        | tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+    echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.gpg] \
+      https://pkg.jenkins.io/debian-stable binary/ \
+      | tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 
     apt-get update -y
     apt-get install -y jenkins
-    
+
     systemctl enable jenkins
     systemctl start jenkins
 
